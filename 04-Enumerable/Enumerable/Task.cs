d8a130f2a76e81faeb1465ce04888fc4e2c35a5e
Artur <khantsevich.artur@gmail.com>
2018-08-10 23:37:46 +0300

    GetSquareSequence

--- 04-Enumerable/Enumerable/Task.cs
+++ 04-Enumerable/Enumerable/Task.cs
@@ -55,8 +55,7 @@ namespace EnumerableTask
         /// </example>
         public IEnumerable<long> GetSquareSequence(IEnumerable<int> data)
         {
-            // TODO : Implement GetSquareSequence
-            throw new NotImplementedException();
+            return data.Select(x => (long)x *x);
         }
 
         /// <summary>Transforms int sequence to its moving sum sequence, 
